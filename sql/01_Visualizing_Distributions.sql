91588ffdc7c83c781363c173a2b5cf6469edd437
Vibes <61502171+VibeHarboe@users.noreply.github.com>
2025-06-07 17:55:17 +0200

    Update 01_Visualizing_Distributions.sql

--- sql/01_Visualizing_Distributions.sql
+++ sql/01_Visualizing_Distributions.sql
@@ -1,1 +1,5 @@
 #
+
+
+
+![Joining Data in SQL](https://github.com/VibeHarboe/Joining-Data-in-SQL/blob/8c550ad556af45bb14591e69105d8ff75c5ca72f/visuals/Joining%20Data%20in%20SQL.png "Joining Data in SQL")
